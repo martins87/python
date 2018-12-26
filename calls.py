e8942e281df2874a25742aacc80b9b60fe6a787b
Daniel Martins <martinsda@gmail.com>
2018-12-26 11:10:28 -0200

    Update calls.py

--- calls.py
+++ calls.py
@@ -11,27 +11,27 @@ print(log_data)
 
 lines = log_data.split('\n')
 
-completed_ocurrences = []
-all_ocurrences = []
+completed_occurrences = []
+all_occurrences = []
 
 for line in lines:
     if len(line) > 0:
         line_data = line.split(',')
 
-        # adds ocurrence to completed call list
+        # adds occurrence to completed call list
         if line_data[-1] == 'COMPLETED':
-            completed_ocurrences.append(line_data[1])
-            completed_ocurrences.append(line_data[2])
+            completed_occurrences.append(line_data[1])
+            completed_occurrences.append(line_data[2])
 
-        all_ocurrences.append(line_data[1])
-        all_ocurrences.append(line_data[2])
+        all_occurrences.append(line_data[1])
+        all_occurrences.append(line_data[2])
 
 # counts number of times each number appears in the list
-completed_dict = dict(Counter(completed_ocurrences))
-all_dict = dict(Counter(all_ocurrences))
+completed_dict = dict(Counter(completed_occurrences))
+all_dict = dict(Counter(all_occurrences))
 
-print('Completed ocurrences: \n{}\n'.format(completed_dict))
-print('All ocurrences: \n{}\n'.format(all_dict))
+print('Completed occurrences: \n{}\n'.format(completed_dict))
+print('All occurrences: \n{}\n'.format(all_dict))
 
 result = {}
 
